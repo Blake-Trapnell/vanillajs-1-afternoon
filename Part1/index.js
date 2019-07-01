64c23471082ddf3fd1ead1fa755ce8eea0b48728
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-07-01 16:47:06 -0600

    update

--- Part1/index.js
+++ Part1/index.js
@@ -1,10 +1,13 @@
 //variables
 let board = []
-let catsGame = true
+let catsGame = 0;
+let winner = ``
+
 //Functions
 
 
 let play = (clickedId)=> {
+    catsGame++
     const playerSpan = document.getElementById(`player`)
     const clickedElement = document.getElementById(clickedId)
     if (clickedElement.innerText === `X` || 
@@ -23,6 +26,7 @@ let play = (clickedId)=> {
         board[2] === `X` && board[5] === `X` && board[8] === "X" ||
         board[0] === `X` && board[4] === `X` && board[8] === "X" ||
         board[2] === `X` && board[4] === `X` && board[6] === "X" ) {
+            winner = `X`
             alert(`X Wins`) 
         }
   }
@@ -38,7 +42,13 @@ let play = (clickedId)=> {
           board[2] === `O` && board[5] === `O` && board[8] === "O" ||
           board[0] === `O` && board[4] === `O` && board[8] === "O" ||
           board[2] === `O` && board[4] === `O` && board[6] === "O" ) {
+              winner = `O`
               alert(`O Wins`) }
+            }
+            console.log(catsGame)
+            console.log(winner)
+            if (catsGame == 9 && winner !== `X` && winner !== `O`) {
+                alert(`tie`)
             }
         }
         // button
@@ -46,12 +56,12 @@ let play = (clickedId)=> {
             for(let i = 0;i <= 8; i++) {
                 let restart = document.getElementById([i]) 
                 restart.innerText = ``
+                catsGame = 0
             } 
             board = []
             console.log(board)
     }
-        
-        
-        
+
+    
 
 
